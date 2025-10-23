c306f6ffbbbdf54742a791ee53380b0854f5461a
otomegan <165517359+otomegan@users.noreply.github.com>
2025-10-23 15:49:32 -0300

    CartasSuperTrunfo.c
    criação do supertrunfo iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,50 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int NumeroDaCarta; // %i ou %d modificador com &
+  char Estado; // %c modificador com &
+  char CodigoDaCarta[5]; // %c modificador
+  char Cidade[10]; // %s modificador com &
+  int Populacao; // %d ou i modificador com &
+  float Areakm2; // %f modificador com &
+  float Pib; // %f  modificador com &
+  int PontosTuristicos; // %d ou %i modificador com &
 
   // Área para entrada de dados
+  printf("Carta: \n");
+    scanf(" %d", &NumeroDaCarta);
+  
+  printf("Estado: \n");
+    scanf(" %c", &Estado);
+  
+  printf("Código: \n");
+    scanf(" %s", CodigoDaCarta);
+
+  printf("Nome da Cidade: ");
+    scanf(" %s", Cidade);
+
+  printf("População: \n");
+    scanf(" %d", &Populacao);
+
+  printf("Área: \n");
+    scanf(" %f", &Areakm2);
+
+  printf("PIB: \n");
+    scanf(" %f", &Pib);
+
+  printf("Número de Pontos Turísticos: \n");
+    scanf(" %d", &PontosTuristicos);
 
   // Área para exibição dos dados da cidade
+
+  printf("Carta: %d\n", NumeroDaCarta);
+  printf("Estado: %c\n", Estado);
+  printf("Código: %s\n", CodigoDaCarta);
+  printf("Nome da Cidade: %s", Cidade);
+  printf("População: %d\n", Populacao);
+  printf("Área: %f\n", Areakm2);
+  printf("PIB: %f\n", Pib);
+  printf("Número de Pontos Turísticos: %i\n", PontosTuristicos);
 
 return 0;
 } 
